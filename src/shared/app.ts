4edc1874da7b18ccd98dd0a9fbce00da985975f7
Gabrielle Monteiro <gabrielle.monteiro95@outlook.com>
2021-10-15 10:49:15 -0300

    feat: adds database connection

--- src/shared/app.ts
+++ src/shared/app.ts
@@ -3,6 +3,9 @@ import Fastify, { FastifyInstance } from 'fastify'
 import * as dotenv from 'dotenv'
 dotenv.config()
 import { healthcheckRoutes } from './routes/healthcheckRoutes'
+import Mongodb from './plugins/mongoose'
+
+new Mongodb();
 
 const app: FastifyInstance = Fastify({})
 
