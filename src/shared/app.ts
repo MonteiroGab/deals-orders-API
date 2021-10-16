eae45a9e22f92819844e1d6337880e08efb0e47b
Gabrielle Monteiro <gabrielle.monteiro95@outlook.com>
2021-10-15 21:13:31 -0300

    feat: adds pipedrive integration

--- src/shared/app.ts
+++ src/shared/app.ts
@@ -2,13 +2,15 @@
 import Fastify, { FastifyInstance } from 'fastify'
 import * as dotenv from 'dotenv'
 dotenv.config()
-import { healthcheckRoutes } from './routes/healthcheckRoutes'
 import Mongodb from './plugins/mongoose'
+import { healthcheckRoutes } from './routes/healthcheckRoutes'
+import { dealsRoutes } from './routes/dealsRoutes'
 
-new Mongodb();
+new Mongodb()
 
 const app: FastifyInstance = Fastify({})
 
 app.register(healthcheckRoutes.params)
+app.register(dealsRoutes.params, { prefix: '/deals' })
 
 export { app }
